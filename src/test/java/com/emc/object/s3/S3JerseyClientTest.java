e1d5ca81e2e228dbb5a034e532fffbd650d6e8e0
Stu Arnett <stu.arnett@emc.com>
2015-01-16 15:30:29 -0600

    object-client: fixed request logging for tests and added concurrent test framework

--- src/test/java/com/emc/object/s3/S3JerseyClientTest.java
+++ src/test/java/com/emc/object/s3/S3JerseyClientTest.java
@@ -1,10 +1,12 @@
 package com.emc.object.s3;
 
+import com.emc.object.AbstractClientTest;
 import com.emc.object.s3.bean.ListBucketsResult;
 import com.emc.object.s3.jersey.S3JerseyClient;
 import com.emc.vipr.services.lib.ViprConfig;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.glassfish.jersey.apache.connector.ApacheClientProperties;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.net.URI;
@@ -12,11 +14,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-public class S3JerseyClientTest {
+public class S3JerseyClientTest extends AbstractClientTest {
     protected S3Client client;
 
-    @Before
-    public void setup() throws Exception {
+    @Override
+    protected void createBucket(String bucketName) throws Exception {
+        client.createBucket(bucketName);
+    }
+
+    @Override
+    protected void cleanUpBucket(String bucketName) throws Exception {
+//        for (String key : client.listObjects(getTestBucket())) {
+//            client.deleteObject(bucketName, key);
+//        }
+        client.deleteBucket(bucketName);
+    }
+
+    @Override
+    public void initClient() throws Exception {
         Properties props = ViprConfig.getProperties();
 
         String accessKey = ViprConfig.getPropertyNotEmpty(props, ViprConfig.PROP_S3_ACCESS_KEY_ID);
@@ -27,6 +42,11 @@ public class S3JerseyClientTest {
         S3Config s3Config = new S3Config();
         s3Config.setEndpoints(parseUris(endpoints == null ? endpoint : endpoints));
         s3Config.withIdentity(accessKey).withSecretKey(secretKey);
+        s3Config.getProperties().put(S3Config.PROPERTY_POLL_PROTOCOL, "http");
+        s3Config.getProperties().put(S3Config.PROPERTY_POLL_PORT, "9020");
+
+        // TODO: remove when jersey request logging is fixed
+        s3Config.getProperties().put(ApacheClientProperties.CONNECTION_MANAGER, new PoolingHttpClientConnectionManager());
 
         client = new S3JerseyClient(s3Config);
     }
@@ -37,6 +57,16 @@ public class S3JerseyClientTest {
         Assert.assertNotNull(result);
         Assert.assertNotNull(result.getOwner());
         Assert.assertNotNull(result.getBuckets());
+    }
+
+    public void sampleTest() throws Exception {
+        // create some objects
+        String key = "my-object";
+//        client.createObject(getTestBucket(), key);
+
+        // test some stuff
+
+        // done (don't have to clean up after yourself)
     }
 
     protected List<URI> parseUris(String uriString) throws Exception {
