919a0cee2eb02f98ccbdf154dbdd3d4c06be23ca
Stu Arnett <stu.arnett@emc.com>
2015-01-19 15:02:37 -0600

    object-client: implemented ListObjectsResult bean (must have missed that)

--- src/main/java/com/emc/object/s3/bean/ListObjectsResult.java
+++ src/main/java/com/emc/object/s3/bean/ListObjectsResult.java
@@ -1,4 +1,110 @@
 package com.emc.object.s3.bean;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import java.util.ArrayList;
+import java.util.List;
+
+@XmlRootElement(name = "ListBucketResult")
 public class ListObjectsResult {
+    private String bucketName;
+    private String prefix;
+    private String delimiter;
+    private Integer maxKeys;
+    private String marker;
+    private String nextMarker;
+    private Boolean truncated;
+    private List<S3Object> objects = new ArrayList<>();
+    private List<CommonPrefix> _commonPrefixes = new ArrayList<>();
+
+    @XmlElement(name = "Name")
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    @XmlElement(name = "Prefix")
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    @XmlElement(name = "Delimiter")
+    public String getDelimiter() {
+        return delimiter;
+    }
+
+    public void setDelimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    @XmlElement(name = "MaxKeys")
+    public Integer getMaxKeys() {
+        return maxKeys;
+    }
+
+    public void setMaxKeys(Integer maxKeys) {
+        this.maxKeys = maxKeys;
+    }
+
+    @XmlElement(name = "Marker")
+    public String getMarker() {
+        return marker;
+    }
+
+    public void setMarker(String marker) {
+        this.marker = marker;
+    }
+
+    @XmlElement(name = "NextMarker")
+    public String getNextMarker() {
+        return nextMarker;
+    }
+
+    public void setNextMarker(String nextMarker) {
+        this.nextMarker = nextMarker;
+    }
+
+    @XmlElement(name = "IsTruncated")
+    public Boolean getTruncated() {
+        return truncated;
+    }
+
+    public void setTruncated(Boolean truncated) {
+        this.truncated = truncated;
+    }
+
+    @XmlElement(name = "Contents")
+    public List<S3Object> getObjects() {
+        return objects;
+    }
+
+    public void setObjects(List<S3Object> objects) {
+        this.objects = objects;
+    }
+
+    @XmlElement(name = "CommonPrefixes")
+    protected List<CommonPrefix> get_commonPrefixes() {
+        return _commonPrefixes;
+    }
+
+    protected void set_commonPrefixes(List<CommonPrefix> _commonPrefixes) {
+        this._commonPrefixes = _commonPrefixes;
+    }
+
+    @XmlTransient
+    public List<String> getCommonPrefixes() {
+        List<String> prefixes = new ArrayList<>();
+        for (CommonPrefix prefix : _commonPrefixes) {
+            prefixes.add(prefix.getPrefix());
+        }
+        return prefixes;
+    }
 }
---
+++ src/main/java/com/emc/object/s3/bean/S3Object.java
@@ -0,0 +1,67 @@
+package com.emc.object.s3.bean;
+
+import javax.xml.bind.annotation.XmlElement;
+import java.util.Date;
+
+public class S3Object {
+    private String key;
+    private Date lastModified;
+    private String eTag;
+    private Long size;
+    private StorageClass storageClass;
+    private Owner owner;
+
+    @XmlElement(name = "Key")
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    @XmlElement(name = "LastModified")
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Date lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    @XmlElement(name = "ETag")
+    public String geteTag() {
+        return eTag;
+    }
+
+    public void seteTag(String eTag) {
+        this.eTag = eTag;
+    }
+
+    @XmlElement(name = "Size")
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    @XmlElement(name = "StorageClass")
+    public StorageClass getStorageClass() {
+        return storageClass;
+    }
+
+    public void setStorageClass(StorageClass storageClass) {
+        this.storageClass = storageClass;
+    }
+
+    @XmlElement(name = "Owner")
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+}
