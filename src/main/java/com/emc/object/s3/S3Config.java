5e328b011b84fe3e88dcba23c27f2c27018d9206
seibed <david.seibert@emc.com>
2016-08-30 14:07:09 -0400

    Copyright date corrections

--- src/main/java/com/emc/object/s3/S3Config.java
+++ src/main/java/com/emc/object/s3/S3Config.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, EMC Corporation.
+ * Copyright (c) 2015-2016, EMC Corporation.
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
