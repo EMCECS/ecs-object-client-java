540999eb5af088c6a41d304aa126c36f767bc38f
arnett, stu <stu.arnett@emc.com>
2018-02-08 12:18:42 -0600

    added automatic single/parallel execution in LargeFileUploader and LargeFileDownloader along with tests. also added fine-grained support for progress indicators (included in tests) fixes github issue #11

--- src/main/java/com/emc/object/s3/LargeFileDownloader.java
+++ src/main/java/com/emc/object/s3/LargeFileDownloader.java
@@ -27,10 +27,14 @@
 package com.emc.object.s3;
 
 import com.emc.object.Range;
-import com.emc.object.s3.request.GetObjectRequest;
+import com.emc.object.util.ProgressInputStream;
+import com.emc.object.util.ProgressListener;
+import com.emc.object.util.ProgressOutputStream;
+import com.emc.rest.util.StreamUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
@@ -39,20 +43,20 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Convenience class to facilitate multi-threaded download for large objects. This class will split the object
  * and download it in parts, transferring several parts simultaneously to maximize efficiency.
  */
-public class LargeFileDownloader implements Runnable {
+public class LargeFileDownloader implements Runnable, ProgressListener {
 
     private static final Logger log = LoggerFactory.getLogger(LargeFileDownloader.class);
 
+    public static final int DEFAULT_PARALLEL_THRESHOLD = 128 * 1024 * 1024; // 128MB
+
     public static final int MIN_PART_SIZE = 2 * 1024 * 1024; // 2MB
-    public static final int DEFAULT_PART_SIZE = 4 * 1024 * 1024; // 4MB
+    public static final int DEFAULT_PART_SIZE = 32 * 1024 * 1024; // 32MB
 
     public static final int DEFAULT_THREADS = 8;
 
@@ -60,9 +64,14 @@ public class LargeFileDownloader implements Runnable {
     private String bucket;
     private String key;
     private File file;
+    private Long objectSize;
+    private AtomicLong bytesTransferred = new AtomicLong();
+
+    private long parallelThreshold = DEFAULT_PARALLEL_THRESHOLD;
     private long partSize = DEFAULT_PART_SIZE;
     private int threads = DEFAULT_THREADS;
     private ExecutorService executorService;
+    private ProgressListener progressListener;
 
     /**
      * Creates a new LargeFileDownloader instance that will use <code>s3Client</code> to download
@@ -76,7 +85,54 @@ public class LargeFileDownloader implements Runnable {
     }
 
     @Override
+    public void progress(long completed, long total) {
+    }
+
+    @Override
+    public void transferred(long size) {
+        long totalTransferred = bytesTransferred.addAndGet(size);
+
+        if (progressListener != null) {
+            progressListener.transferred(size);
+            progressListener.progress(totalTransferred, objectSize);
+        }
+    }
+
+    @Override
     public void run() {
+        download();
+    }
+
+    /**
+     * This method will automatically choose between parallel and single-GET operations based on a configured threshold.
+     * Note the default threshold is {@link #DEFAULT_PARALLEL_THRESHOLD}. Also note that the defaults in this class are
+     * optimized for high-speed LAN connectivity. When operating over a WAN or a slower connection, you should reduce
+     * the {@link #setParallelThreshold(long)} parallel threshold} and {@link #setPartSize(long) part size}
+     * proportionately.
+     */
+    public void download() {
+        try {
+            // get object metadata (for size)
+            S3ObjectMetadata metadata = s3Client.getObjectMetadata(bucket, key);
+            objectSize = metadata.getContentLength();
+            if (objectSize >= parallelThreshold)
+                doParallelDownload();
+            else
+                doSingleDownload();
+        } catch (Exception e) {
+            throw new RuntimeException("error downloading file", e);
+        }
+    }
+
+    protected void doSingleDownload() throws IOException {
+        OutputStream os = new FileOutputStream(file);
+
+        os = new ProgressOutputStream(os, this);
+
+        StreamUtil.copy(s3Client.readObjectStream(bucket, key, null), os, objectSize);
+    }
+
+    protected void doParallelDownload() throws Exception {
         // sanity checks
         if (file.exists() && !file.canWrite())
             throw new IllegalArgumentException("cannot write to file: " + file.getPath());
@@ -87,12 +143,12 @@ public class LargeFileDownloader implements Runnable {
             partSize = MIN_PART_SIZE;
         }
 
-        // get object metadata (for size)
-        S3ObjectMetadata objectMetadata = s3Client.getObjectMetadata(bucket, key);
-        long objectSize = objectMetadata.getContentLength();
-
         // set up thread pool
-        if (executorService == null) executorService = Executors.newFixedThreadPool(threads);
+        boolean shutdownThreadPool = false;
+        if (executorService == null) {
+            executorService = Executors.newFixedThreadPool(threads);
+            shutdownThreadPool = true;
+        }
         List<Future<Void>> futures = new ArrayList<Future<Void>>();
 
         try {
@@ -117,14 +173,10 @@ public class LargeFileDownloader implements Runnable {
 
             // close file
             raFile.close();
-
-        } catch (Exception e) {
-            if (e instanceof RuntimeException) throw (RuntimeException) e;
-            throw new RuntimeException("error downloading file", e);
         } finally {
 
             // make sure all spawned threads are shut down
-            executorService.shutdown();
+            if (shutdownThreadPool) executorService.shutdown();
         }
     }
 
@@ -144,13 +196,34 @@ public class LargeFileDownloader implements Runnable {
         return file;
     }
 
+    public Long getObjectSize() {
+        return objectSize;
+    }
+
+    public long getBytesTransferred() {
+        return bytesTransferred.get();
+    }
+
+    public long getParallelThreshold() {
+        return parallelThreshold;
+    }
+
+    /**
+     * Sets the threshold above which parallel operations are used to download, and below which a single-GET is used.
+     * This only applies when using the {@link #download()} method. Note the default threshold is
+     * {@link #DEFAULT_PARALLEL_THRESHOLD}
+     */
+    public void setParallelThreshold(long parallelThreshold) {
+        this.parallelThreshold = parallelThreshold;
+    }
+
     public long getPartSize() {
         return partSize;
     }
 
     /**
      * Sets the size of each part to download. Note that 1MB is the minimum part size and
-     * the default is 5MB.
+     * the default is {@link #DEFAULT_PART_SIZE}.
      */
     public void setPartSize(long partSize) {
         this.partSize = partSize;
@@ -180,6 +253,39 @@ public class LargeFileDownloader implements Runnable {
         this.executorService = executorService;
     }
 
+    public ProgressListener getProgressListener() {
+        return progressListener;
+    }
+
+    public void setProgressListener(ProgressListener progressListener) {
+        this.progressListener = progressListener;
+    }
+
+    public LargeFileDownloader withParallelThreshold(long parallelThreshold) {
+        setParallelThreshold(parallelThreshold);
+        return this;
+    }
+
+    public LargeFileDownloader withPartSize(long partSize) {
+        setPartSize(partSize);
+        return this;
+    }
+
+    public LargeFileDownloader withThreads(int threads) {
+        setThreads(threads);
+        return this;
+    }
+
+    public LargeFileDownloader withExecutorService(ExecutorService executorService) {
+        setExecutorService(executorService);
+        return this;
+    }
+
+    public LargeFileDownloader withProgressListener(ProgressListener progressListener) {
+        setProgressListener(progressListener);
+        return this;
+    }
+
     protected class DownloadPartTask implements Callable<Void> {
         private Range range;
         private FileChannel channel;
@@ -191,9 +297,26 @@ public class LargeFileDownloader implements Runnable {
 
         @Override
         public Void call() throws Exception {
-            byte[] data = s3Client.getObject(new GetObjectRequest(bucket, key).withRange(range), byte[].class).getObject();
-            channel.write(ByteBuffer.wrap(data), range.getFirst());
-            return null;
+            InputStream is = s3Client.readObjectStream(bucket, key, range);
+
+            try {
+                is = new ProgressInputStream(is, LargeFileDownloader.this);
+
+                byte[] buffer = new byte[32 * 1024];
+                long pos = range.getFirst();
+                for (int r = 0; r != -1; r = is.read(buffer)) {
+                    channel.write(ByteBuffer.wrap(buffer, 0, r), pos);
+                    pos += r;
+                }
+
+                return null;
+            } finally {
+                try {
+                    is.close();
+                } catch (Throwable t) {
+                    log.warn("could not close object stream", t);
+                }
+            }
         }
     }
 }
---
+++ src/test/java/com/emc/object/s3/LargeFileDownloaderTest.java
@@ -0,0 +1,169 @@
+/*
+ * Copyright (c) 2015-2018, EMC Corporation.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * + Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * + Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * + The name of EMC Corporation may not be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.emc.object.s3;
+
+import com.emc.object.s3.jersey.S3JerseyClient;
+import com.emc.object.util.ProgressListener;
+import com.emc.rest.util.StreamUtil;
+import com.emc.util.RandomInputStream;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class LargeFileDownloaderTest extends AbstractS3ClientTest {
+    static final long FILE_SIZE = 20 * 1024 * 1024; // 20MB
+
+    DigestInputStream dis;
+    File sourceFile;
+    File destFile;
+    String md5Hex;
+    String key;
+
+    @Override
+    protected String getTestBucketPrefix() {
+        return "lfd-test";
+    }
+
+    @Override
+    protected S3Client createS3Client() throws Exception {
+        return new S3JerseyClient(createS3Config());
+    }
+
+    @Before
+    public void createObject() throws Exception {
+        sourceFile = File.createTempFile("lfd-source", null);
+        sourceFile.deleteOnExit();
+        destFile = File.createTempFile("lfd-dest", null);
+        destFile.deleteOnExit();
+
+        // create temp file
+        dis = new DigestInputStream(new RandomInputStream(FILE_SIZE), MessageDigest.getInstance("MD5"));
+        StreamUtil.copy(dis, new FileOutputStream(sourceFile), FILE_SIZE);
+        md5Hex = DatatypeConverter.printHexBinary(dis.getMessageDigest().digest()).toLowerCase();
+
+        // upload using LFU (this is done to save time)
+        key = "lfd-test";
+        LargeFileUploader lfu = new LargeFileUploader(client, getTestBucket(), key, sourceFile).withPartSize(FILE_SIZE / 5);
+        lfu.doMultipartUpload();
+    }
+
+    @Test
+    public void testAboveThreshold() throws Exception {
+        long partSize = FILE_SIZE / 5;
+        final AtomicLong bytesTransferred = new AtomicLong(), bytesCompleted = new AtomicLong(), bytesTotal = new AtomicLong();
+
+        // downlaod in 4MB chunks
+        LargeFileDownloader lfd = new LargeFileDownloader(client, getTestBucket(), key, destFile);
+        lfd.withParallelThreshold(FILE_SIZE).withPartSize(partSize);
+        lfd.setProgressListener(new ProgressListener() {
+            @Override
+            public void progress(long completed, long total) {
+                bytesCompleted.set(completed);
+                bytesTotal.set(total);
+            }
+
+            @Override
+            public void transferred(long size) {
+                bytesTransferred.addAndGet(size);
+            }
+        });
+        lfd.download();
+
+        // verify threads were used (this will be null if single-GET)
+        Assert.assertNotNull(lfd.getExecutorService());
+
+        // verify progress indicators
+        Assert.assertEquals(FILE_SIZE, lfd.getBytesTransferred());
+        Assert.assertEquals(FILE_SIZE, bytesCompleted.get());
+        Assert.assertEquals(FILE_SIZE, bytesTotal.get());
+        Assert.assertEquals(FILE_SIZE, bytesTransferred.get());
+
+        // verify content
+        DigestInputStream dis = new DigestInputStream(new FileInputStream(destFile), MessageDigest.getInstance("MD5"));
+        StreamUtil.copy(dis, new NullStream(), destFile.length());
+        Assert.assertEquals(md5Hex, DatatypeConverter.printHexBinary(dis.getMessageDigest().digest()).toLowerCase());
+    }
+
+    @Test
+    public void testBelowThreshold() throws Exception {
+        final AtomicLong bytesTransferred = new AtomicLong(), bytesCompleted = new AtomicLong(), bytesTotal = new AtomicLong();
+
+        // download in single stream
+        LargeFileDownloader lfd = new LargeFileDownloader(client, getTestBucket(), key, destFile);
+        lfd.withParallelThreshold(FILE_SIZE + 1);
+        lfd.setProgressListener(new ProgressListener() {
+            @Override
+            public void progress(long completed, long total) {
+                bytesCompleted.set(completed);
+                bytesTotal.set(total);
+            }
+
+            @Override
+            public void transferred(long size) {
+                bytesTransferred.addAndGet(size);
+            }
+        });
+        lfd.download();
+
+        // verify single thread (thread pool should be null)
+        Assert.assertNull(lfd.getExecutorService());
+
+        // verify progress indicators
+        Assert.assertEquals(FILE_SIZE, lfd.getBytesTransferred());
+        Assert.assertEquals(FILE_SIZE, bytesCompleted.get());
+        Assert.assertEquals(FILE_SIZE, bytesTotal.get());
+        Assert.assertEquals(FILE_SIZE, bytesTransferred.get());
+
+        // verify content
+        DigestInputStream dis = new DigestInputStream(new FileInputStream(destFile), MessageDigest.getInstance("MD5"));
+        StreamUtil.copy(dis, new NullStream(), destFile.length());
+        Assert.assertEquals(md5Hex, DatatypeConverter.printHexBinary(dis.getMessageDigest().digest()).toLowerCase());
+    }
+
+    class NullStream extends OutputStream {
+        @Override
+        public void write(int b) {
+        }
+
+        @Override
+        public void write(byte[] b) {
+        }
+
+        @Override
+        public void write(byte[] b, int off, int len) {
+        }
+    }
+}
