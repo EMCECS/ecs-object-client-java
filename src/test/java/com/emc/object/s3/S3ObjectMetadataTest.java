e0c9a0577b964bb889637a967b781082143bfa75
arnett, stu <stu.arnett@emc.com>
2018-03-13 10:50:38 -0500

    changed user metadata map in S3ObjectMetadata to be case-insensitive to provide compatibility between ECS versions that preserve / do not preserve case

---
+++ src/test/java/com/emc/object/s3/S3ObjectMetadataTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2015-2018, EMC Corporation.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * + Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * + Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * + The name of EMC Corporation may not be used to endorse or promote
+ *   products derived from this software without specific prior written
+ *   permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.emc.object.s3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class S3ObjectMetadataTest {
+    @Test
+    public void testCaseInsensitivity() {
+        S3ObjectMetadata metadata = new S3ObjectMetadata()
+                .addUserMetadata("One", "Two")
+                .addUserMetadata("one", "three")
+                .addUserMetadata("ONE", "FOUR")
+
+                .addUserMetadata("Five", "Six")
+                .addUserMetadata("five", "seven")
+                .addUserMetadata("FIVE", "EIGHT");
+
+        // TreeMap will sort the keys and the first insertion case is preserved
+        Assert.assertArrayEquals(new String[]{"Five", "One"}, metadata.getUserMetadata().keySet().toArray());
+        Assert.assertEquals("FOUR", metadata.getUserMetadata("one"));
+        Assert.assertEquals("FOUR", metadata.getUserMetadata("One"));
+        Assert.assertEquals("FOUR", metadata.getUserMetadata("oNe"));
+        Assert.assertEquals("EIGHT", metadata.getUserMetadata("five"));
+        Assert.assertEquals("EIGHT", metadata.getUserMetadata("Five"));
+        Assert.assertEquals("EIGHT", metadata.getUserMetadata("fIve"));
+    }
+}
