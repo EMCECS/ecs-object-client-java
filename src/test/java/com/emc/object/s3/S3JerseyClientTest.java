6b9e90f1aca26d3f961da301aa850d8f31b12eec
Billy_Yuan <billy.yuan.work@gmail.com>
2023-07-19 11:08:44 +0800

    fix test env

--- src/test/java/com/emc/object/s3/S3JerseyClientTest.java
+++ src/test/java/com/emc/object/s3/S3JerseyClientTest.java
@@ -2817,7 +2817,7 @@ public class S3JerseyClientTest extends AbstractS3ClientTest {
     }
 
     @Test
-    public void testCopyRangeAPI() {
+    public void testCopyRangeAPI() throws InterruptedException {
         Assume.assumeTrue("ECS version must be at least 3.6.2", ecsVersion != null && ecsVersion.compareTo("3.6.2") >= 0);
         Assume.assumeFalse("Copy range API is not supported with IAM user.", isIamUser);
 
@@ -3055,6 +3055,9 @@ public class S3JerseyClientTest extends AbstractS3ClientTest {
                 Assert.assertEquals(404, es.getHttpCode());
             }
         }
+
+        // avoid "The object is under retention and can't be deleted or modified" when cleaning the test bucket
+        Thread.sleep(2000);
     }
 
     protected void assertAclEquals(AccessControlList acl1, AccessControlList acl2) {
